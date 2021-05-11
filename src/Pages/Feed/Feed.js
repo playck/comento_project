4413248f0c5e0068b545c2793785a920fd0c336f
playck <playck@naver.com>
2021-05-11 18:18:18 +0900

    Add: login button, filter button UI

--- src/Pages/Feed/Feed.js
+++ src/Pages/Feed/Feed.js
@@ -1,7 +1,21 @@
 import React from "react";
+import "./Feed.scss";
 
-const Main = () => {
-  return <div></div>;
+const Feed = () => {
+  return (
+    <div className="FeedMain">
+      <button className="loginBtn">로그인</button>
+      <div className="Feed">
+        <div className="orderBtn">
+          <div>
+            <span className="active">∙ 오름차순 &nbsp;</span>
+            <span>∙ 내림차순</span>
+          </div>
+          <button className="filterBtn">필터</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default Main;
+export default Feed;
