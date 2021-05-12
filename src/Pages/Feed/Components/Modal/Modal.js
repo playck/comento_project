4f891e7b71870211112735adc1dd4616c8befb3a
playck <playck@naver.com>
2021-05-13 00:31:50 +0900

    Modify: Feed Page 반응형 UI

---
+++ src/Pages/Feed/Components/Modal/Modal.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Modal = () => {
+  return (
+    <div className="Modal">
+      <div>
+        <input />
+        <input />
+        <input />
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
--- src/store/modules/feedList.js
+++ src/store/modules/feedList.js
@@ -2,21 +2,25 @@ import axios from "axios";
 import { handleActions } from "redux-actions";
 import { SERVER } from "../../config";
 
-const getFeedListDataAPI = () => axios.get(`${SERVER}/api/list`);
+const getFeedListDataAPI = (page, ord, category, limit) =>
+  axios.get(
+    `${SERVER}/api/list?page=${page}&limit=${limit}&ord=${ord}${category}`
+  );
 
 const GET_FEED_LIST_DATA = "GET_PRODUCT_LIST_DATA";
 const GET_FEED_LIST_DATA_SUCEESS = "GET_PRODUCT_LIST_DATA_SUCEESS";
 const GET_FEED_LIST_DATA_FAILURE = "GET_PRODUCT_LIST_DATA_FAILURE";
 
-export const getFeedListData = () => async (dispatch) => {
-  dispatch({ type: GET_FEED_LIST_DATA });
-  try {
-    const res = await getFeedListDataAPI();
-    dispatch({ type: GET_FEED_LIST_DATA_SUCEESS, payload: res.data });
-  } catch (e) {
-    dispatch({ type: GET_FEED_LIST_DATA_FAILURE, payload: e });
-  }
-};
+export const getFeedListData =
+  (page, ord, category, limit) => async (dispatch) => {
+    dispatch({ type: GET_FEED_LIST_DATA });
+    try {
+      const res = await getFeedListDataAPI(page, ord, category, limit);
+      dispatch({ type: GET_FEED_LIST_DATA_SUCEESS, payload: res.data });
+    } catch (e) {
+      dispatch({ type: GET_FEED_LIST_DATA_FAILURE, payload: e });
+    }
+  };
 
 const initState = {
   feedList: "",
