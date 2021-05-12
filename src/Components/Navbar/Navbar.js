140034a05c8fd00d31c66f6f54097705d0dd3fd1
playck <playck@naver.com>
2021-05-12 16:24:43 +0900

    Add: Navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,7 +1,12 @@
 import React from "react";
+import "./Navbar.scss";
 
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <nav className="navbar">
+      <div>[제출일] 이름을 작성해 주세요</div>
+    </nav>
+  );
 };
 
 export default Navbar;
--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar/Navbar";
 import FeedDetail from "./Pages/FeedDetail/FeedDetail";
 import Feed from "./Pages/Feed/Feed";
 
@@ -7,6 +8,7 @@ export default class Routes extends Component {
   render() {
     return (
       <Router>
+        <Navbar />
         <Switch>
           <Route exact path="/" component={Feed} />
           <Route path="/:id" component={FeedDetail} />
