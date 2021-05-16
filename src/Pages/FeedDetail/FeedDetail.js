66656be874ba69b463a4ed89e0289522bef361b2
playck <playck@naver.com>
2021-05-16 16:11:24 +0900

    Add: Detail Page UI

--- src/Pages/FeedDetail/FeedDetail.js
+++ src/Pages/FeedDetail/FeedDetail.js
@@ -1,7 +1,30 @@
 import React from "react";
+import "./FeedDetail.scss";
 
 const Detail = () => {
-  return <div></div>;
+  return (
+    <div className="detail">
+      <article className="feedCard">
+        <h1 className="title">Title</h1>
+        <p className="content">
+          contentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontent
+        </p>
+        <div className="createDay">2020-05-20</div>
+      </article>
+      <div className="answerInfo">
+        답변 <span>2</span>
+      </div>
+
+      <article className="answerCard">
+        <h2>userName</h2>
+        <div className="underBar"></div>
+        <p>
+          contentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontent
+        </p>
+        <div className="createDay">2020-05-20</div>
+      </article>
+    </div>
+  );
 };
 
 export default Detail;
